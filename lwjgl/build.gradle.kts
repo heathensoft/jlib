687e664a468444f3de9bc9c017beb7c98b9c1204
Frederik Dahl <frederikdahl13@gmail.com>
2024-09-20 23:40:47 +0200

    After LWJGL 3.3.1 -> 3.3.4

--- lwjgl/build.gradle.kts
+++ lwjgl/build.gradle.kts
@@ -6,8 +6,8 @@ repositories { }
 
 dependencies {
     implementation(project(":common"))
-    api(files("../libs/lwjgl-3.3.1/lwjgl.jar"))
-    api(files("../libs/lwjgl-3.3.1/lwjgl-glfw.jar"))
-    api(files("../libs/lwjgl-3.3.1/lwjgl-opengl.jar"))
-    api(files("../libs/lwjgl-3.3.1/lwjgl-stb.jar"))
+    api(files("../libs/lwjgl-3.3.4/lwjgl.jar"))
+    api(files("../libs/lwjgl-3.3.4/lwjgl-glfw.jar"))
+    api(files("../libs/lwjgl-3.3.4/lwjgl-opengl.jar"))
+    api(files("../libs/lwjgl-3.3.4/lwjgl-stb.jar"))
 }
